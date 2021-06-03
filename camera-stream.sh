d39c1c8e1e810a3712769d07217634a5110364e6
SealedJoy <sealedjoy@gmail.com>
2021-06-03 20:23:25 +0100

    add mqtt

--- camera-stream.sh
+++ camera-stream.sh
@@ -2,21 +2,23 @@
 #DISPLAY=:0
 #KITTY_ENABLE_WAYLAND=1
 WAYLAND_DISPLAY=wayland-1
+stream_hostname="192.168.1.12"
+backup_stream="http://deb.lan:8123/lovelace/0"
+stream="http://192.168.1.12:8081"
  
 play /home/sealyj/Music/PremiumBeat_0046_sci_fi_beeps_4.wav &>/dev/null  
 #### TEST ROUTE ####
-ping -W 0.6 -c 1 192.168.1.105 >/dev/null                     # Try once.
+ping -W 0.6 -c 1 bg_cam_pi >/dev/null                     # Try once.
 rc=$?
 if [[ $rc -eq 0 ]] ; then
 	#camera direct stream contactable
-	launch_cam_command="mpv --video-reversal-buffer=240MiB --vid=1 --framedrop=decoder+vo --no-correct-pts --fps=8 --wayland-app-id=summoned_tile --window-scale=0.4 http://192.168.1.105:8081"
+	launch_cam_command="mpv --no-cache --video-reversal-buffer=240MiB --vid=1 --framedrop=vo --no-correct-pts --fps=7 --wayland-app-id=summoned_tile --window-scale=0.4 $stream"
 	stop_cam_command="killall -SIGUSR1 mpv"
 	sleep_time=12
 else
 	echo "launching through browser"
 	#take longer route
-	#launch_cam_command="firefox -P camera_stream -width 60 -height 60 -new-window --kiosk http://192.168.1.145:8123/lovelace/0; sway fullscreen toggle"
-	launch_cam_command="qutebrowser --target=window http://192.168.1.145:8123/lovelace/0"
+	launch_cam_command="qutebrowser --target=window $backup_stream"
 	stop_cam_command="killall -SIGUSR1 qutebrowser"
 	sleep_time=25
 fi
