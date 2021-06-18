069d9f2f2fc29229e4d0a9d8f3e3c39b8387b448
SealedJoy <sealedjoy@gmail.com>
2021-06-18 16:42:27 +0100

    add tmux spawn small session

---
+++ tmuxytfzfcontrol.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+DISPLAY=:0
+tmux splitw "cava"
+tmux splitw "ytfzf -m -s --sort -A -a"
---
+++ ytfzf_tmux.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+tmux new-session -s 'Music' -n "Find Music:" $HOME/projects/sway/tmuxytfzfcontrol.sh
